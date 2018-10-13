c7415e7f5a562a57a2c496b96370a6789d5ae0bd
xw <daysme@gmail.com>
2018-10-13 23:57:49 +0800

    feat: 初始化项目及 call 页面

--- src/tool/index.js
+++ src/tool/index.js
@@ -1,16 +1,16 @@
 export default {
   storage: {
-    //存储
-    set(key, value) {
-      localStorage.setItem(key, JSON.stringify(value));
+    // 存储
+    set (key, value) {
+      window.localStorage.setItem(key, JSON.stringify(value))
     },
-    //取出数据
-    get(key) {
-      return JSON.parse(localStorage.getItem(key));
+    // 取出数据
+    get (key) {
+      return JSON.parse(window.localStorage.getItem(key))
     },
     // 删除数据
-    remove(key) {
-      localStorage.removeItem(key);
+    remove (key) {
+      window.localStorage.removeItem(key)
     }
   }
 }
