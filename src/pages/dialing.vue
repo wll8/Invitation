044506bd13fd46f98eb0517ac7ab5e72253407da
wll8 <xwdaysme@gmail.com>
2018-12-08 00:07:43 +0800

    feat: 通话页

---
+++ src/pages/dialing.vue
@@ -0,0 +1,270 @@
+<template>
+  <div class="page_dialing">
+    <bgimg color="#fff" :animate="false"/>
+    <div class="topBar">
+      <div class="tabBox">
+        <div class="tab cur">所有通话</div><div class="tab">未接来电</div>
+      </div>
+    </div>
+    <div class="telBox">
+      <a :href="`tel:${item.tel}`" :class="['telItem', item.class]" v-for="item in [{name: '新郎', tel: 18868875314, class: 'animation1'}, {name: '新娘', tel: 18868875314, class: 'animation2'}]">
+        <div class="member">
+          <div class="text name">新郎</div>
+          <div class="text dev">手机</div>
+        </div>
+        <div class="time">
+          <!-- 距离 call 页面的相对时间 -->
+          <div class="timeText">刚刚</div>
+          <div class="icon info"></div>
+        </div>
+      </a>
+    </div>
+    <div class="footBar">
+      <div class="icon">
+        <div class="iconImg" :style="`background-image: url('${require('../assets/images/star.png')}');`"></div>
+        <div class="iconText">个人收藏</div>
+      </div>
+      <div class="icon">
+        <div class="iconImg" :style="`background-image: url('${require('../assets/images/time.png')}')`"></div>
+        <div class="iconText cur">最近通话</div>
+      </div>
+      <div class="icon">
+        <div class="iconImg" :style="`background-image: url('${require('../assets/images/man.png')}');`"></div>
+        <div class="iconText">通讯录</div>
+      </div>
+    </div>
+    <!-- <Back /> -->
+    <!-- <img class="return" :src="returnImg" :click="() => 1" /> -->
+    <audio class="hidden" id="dialing-audio">
+      <source :src="audioMp3" type="audio/mpeg" />
+    </audio>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'page_dialing',
+  data() {
+    return {
+      audioMp3: require('../assets/audio/dong.mp3'),
+      bgImg: require('../assets/images/bg2.jpg'),
+      returnImg: require('../assets/images/return.png'),
+      itemImg: require('../assets/images/phone-item.jpg'),
+    }
+  },
+  computed: {},
+  created() {},
+  mounted() {
+    setTimeout(()=> {
+      this.$tool.autoPlay('dialing-audio')
+      document.getElementById('dialing-audio').play()
+    }, 100)
+  },
+  methods: {},
+  components: {}
+};
+</script>
+
+<style lang="less">
+@import "../assets/css/util.less";
+
+.page_dialing {
+  background-color: #fff;
+  .topBar {
+    height: 50px;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-bottom: 1px solid #c4c4c4;
+    background-color: #f9f9f9;
+    .tabBox {
+      // width: 100px;
+      text-align: center;
+      padding-top: 10px;
+      .tab {
+        display: inline-block;
+        height: 30px;
+        line-height: 30px;
+        background-color: #fff;
+        color: #007aff;
+        border: 1px solid #007aff;
+        padding: 0 10px;
+        &.cur {
+          background-color: #007aff;
+          color: #fff;
+        }
+        &:first-child {
+          border-top-left-radius: 6px;
+          border-bottom-left-radius: 6px;
+        }
+        &:last-child {
+          border-top-right-radius: 6px;
+          border-bottom-right-radius: 6px;
+        }
+      }
+    }
+  }
+
+  .telBox {
+    height: 50px;
+    width: 100%;
+    position: absolute;
+    top: 50px;
+    left: 0;
+    z-index: 999;
+    .telItem {
+      .transform(translateX(100%));
+      &.animation1 {
+        .animation(dialing-item-animate 0.6s 0.2s ease-out forwards);
+      }
+      &.animation2 {
+        .animation(dialing-item-animate 0.6s 0.8s ease-out forwards);
+      }
+      display: block;
+      height: 48px;
+      line-height: 50px;
+      padding-left: 10%;
+      padding-top: 4px;
+      padding-bottom: 6px;
+      width: 90%;
+      &::after {
+        display: block;
+        content: '';
+        width: 22px;
+        height: 22px;
+        position: absolute;
+        top: 20px;
+        left: 10px;
+        .mbg(cover, '../assets/images/phone.png');
+        background-image: url('../assets/images/phone.png');
+      }
+
+      &::before {
+        width: 90%;
+        left: 10%;
+        position: absolute;
+        display: inline-block;
+        content: '';
+        height: 0;
+        border-bottom: 1px solid #e5e5e5;
+        bottom: 0;
+      }
+
+      .member {
+        float: left;
+        color: #333333;
+        position: relative;
+        .name {
+          height: 30px;
+          line-height: 30px;
+          padding-top: 2px;
+          font-size: 20px;
+          font-family: FZKaTong;
+        }
+        .dev {
+          height: 18px;
+          line-height: 14px;
+          font-size: 12px;
+          text-indent: 2px;
+        }
+      }
+      .time {
+        float: right;
+        padding-right: 23px;
+        position: relative;
+        .timeText {
+          display: inline-block;
+          vertical-align: middle;
+          margin-right: 6px;
+          font-size: 14px;
+          color: #8a8a8a;
+        }
+        .icon.info {
+          display: inline-block;
+          vertical-align: middle;
+          width: 24px;
+          height: 24px;
+          .mbg;
+          background-image: url('../assets/images/info.png');
+        }
+      }
+    }
+  }
+
+  .footBar {
+    border-top: 1px solid #c4c4c4;
+    position: absolute;
+    bottom: 0;
+    width: 60%;
+    padding-right: 40%;
+    padding-bottom: 4px;
+    padding-top: 4px;
+    background: #f9f9f9;
+
+    .icon {
+
+      width: 33%;
+      float: left;
+      text-align: center;
+      .iconImg {
+        width: 40px;
+        height: 40px;
+        // background-color: rgba(1,1,1,.1);
+        display: inline-block;
+        .mbg;
+      }
+      .iconText {
+        font-size: 12px;
+        color: #929292;
+      }
+      .iconText.cur {
+        color: #007aff;
+      }
+    }
+  }
+
+
+  .dialing-item {
+    position: absolute;
+    width: 100%;
+    .transform(translateX(640px));
+
+    .img {
+      width: 100%;
+    }
+    p {
+      position: absolute;
+      font-size: 28px;
+      font-family: FZKaTong;
+      top: 15px;
+      left: 48px;
+      color: rgb(51, 51, 51);
+    }
+  }
+  .dialing-item-1 {
+    top: 81px;
+    .animation(dialing-item-animate 0.6s 0.2s ease-out forwards);
+  }
+
+  .dialing-item-2 {
+    top: 175px;
+    .animation(dialing-item-animate 0.6s 0.8s ease-out forwards);
+  }
+
+  @keyframes dialing-item-animate {
+    from {
+      .transform(translateX(640px));
+    }
+    to {
+      .transform(translateX(0px));
+    }
+  }
+  .return {
+    position: absolute;
+    bottom: 0;
+    right: 20px;
+  }
+}
+</style>
