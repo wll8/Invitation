f00333a3926fd07b03c95cdcd4a45ff0589b7bf3
wll8 <xwdaysme@gmail.com>
2020-04-21 13:13:10 +0800

    chore: 添加示例配置信息, 例如 mock 随机占位图; 添加基本示例媒体资料, 例如声音

--- src/cfg.js
+++ src/cfg.js
@@ -1,3 +1,20 @@
+import tool from './tool/index.js'
+const {simpleSvgPlaceholder, randomColor} = tool
+const randomImg = function randomImg(cfg) {
+  const img = simpleSvgPlaceholder({
+    width: 750,
+    height: 1400,
+    bgColor: `#${randomColor()}`,
+    textColor: `#${randomColor()}`,
+    ...cfg,
+  })
+  console.log(`simpleSvgPlaceholder`, img)
+  return img
+}
+
+
+console.log(`randomColor()`, randomColor())
+
 export default {
   apiUrl: 'http://192.168.18.109:3001/', // 服务器接口地址
   pw: 'xwlove', // 调试页授权码
@@ -7,48 +24,48 @@ export default {
     girl: 'girl', // 在网址参数上添加 ?t=girl 可切换成女生版邀请
   },
   video: {
-    pic: require('./assets/images/play_640.jpg'), // 封面图片
+    pic: randomImg({width: 640, height: 362}), // 封面图片
     url: require('./assets/video/video.mp4'), // 视频文件
   }, // 视频
   photos: [ // 相册， 建议尺寸 750×1334 px
     // img 图片地址
     // pos 偏移 top|right|bottom|left ， 如果图片焦点逼近右边时， 可以使用 right 值保证焦点可见
-    require('./assets/images/my/13.jpg'),
-    require('./assets/images/my/18.jpg'),
-    require('./assets/images/my/10.jpg'),
-    require('./assets/images/my/11.jpg'),
-    require('./assets/images/my/9.jpg'),
-    require('./assets/images/my/5.jpg'),
-    require('./assets/images/my/6.jpg'),
-    require('./assets/images/my/14.jpg'),
-    // {pos: 'top', img: require('./assets/images/my/15.jpg')},
-    require('./assets/images/my/16.jpg'),
-    require('./assets/images/my/21.jpg'),
-    require('./assets/images/my/25.jpg'),
-    require('./assets/images/my/4.jpg'),
-    require('./assets/images/my/1.jpg'),
-    require('./assets/images/my/7.jpg'),
-    require('./assets/images/my/2.jpg'),
-    require('./assets/images/my/8.jpg'),
-    {pos: 'top', img: require('./assets/images/my/3.jpg')},
-    require('./assets/images/my/17.jpg'),
-    require('./assets/images/my/19.jpg'),
-    require('./assets/images/my/20.jpg'),
-    {pos: 'bottom', img: require('./assets/images/my/23.jpg')},
-    require('./assets/images/my/24.jpg'),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    // {pos: 'top', img: randomImg()},
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    {pos: 'top', img: randomImg()},
+    randomImg(),
+    randomImg(),
+    randomImg(),
+    {pos: 'bottom', img: randomImg()},
+    randomImg(),
   ],
   pageBg: {
     // 页面背景图片，为空时显示渐变背景色
-    call: {src: require('./assets/images/my/14.jpg'), animate: true}, // 首页(来电页)
-    talk: {src: require('./assets/images/my/18.jpg'), animate: true}, // 电话接听页
-    desktop: {src: require('./assets/images/my/6.jpg'), animate: true}, // 桌面
-    blessing: require('./assets/images/my/1.jpg'), // 祝福
+    call: {src: randomImg(), animate: true}, // 首页(来电页)
+    talk: {src: randomImg(), animate: true}, // 电话接听页
+    desktop: {src: randomImg(), animate: true}, // 桌面
+    blessing: randomImg(), // 祝福
     dialing: {color: '#fff'}, // 通话记录
     wechat: {color: '#fff'}, // 微信
     photos: require('./assets/images/photos-bg.jpg'), // 相册
-    photograph: require('./assets/images/my/9.jpg'), // 拍照
-    integrated: require('./assets/images/my/5.jpg'), // 集成
-    invite: require('./assets/images/my/15.jpg'), // 邀请
+    photograph: randomImg(), // 拍照
+    integrated: randomImg(), // 集成
+    invite: randomImg(), // 邀请
     map: '', // 地图
   },
   pageMp3Base: { // 默认背景音乐
@@ -78,46 +95,33 @@ export default {
     map: '', // 地图
   },
   date: { // 宴席日期
-    boy: '01/01/2019 15:30:00',
-    girl: '01/01/2019 15:30:00',
+    boy: `${(new Date).getFullYear()+1}-05-20 15:30:00`,
+    girl: `${(new Date).getFullYear()+1}-05-20 12:30:00`,
   },
   inviteText: { // 邀约文本， 空格和换行原样显示
-    title: '金龙 & 蛋蛋的邀约',
+    title: '小龙女 & 杨过的邀约',
     boy: `
-各位亲朋好友：
-
-　　诚挚邀请您于2017年1月31日，星期二，08:00参加金龙和蛋蛋的结婚典礼。
-
-　　您的到来是对我们最好的祝福，敬备喜宴，恭请光临。
-
-　　杭州的小伙伴，年后回杭另设宴席。
+举案齐眉心中想，白头偕老我所愿，执子之手永不变，誓言今朝终实现，百年好合盼祝愿，
+今邀好友来捧场，一定赏光勿迟到，好酒好菜管到饱，
+欢乐开心婚姻好。
     `,
     girl: `
-各位亲朋好友：
-
-　　诚挚邀请您于2017年1月31日，星期二，08:00参加金龙和蛋蛋的结婚典礼。
-
-　　诚挚邀请您于2017年2月4日，星期六，11:00参加金龙和蛋蛋的结婚典礼。
-
-　　杭州的小伙伴，年后回杭另设宴席。
+红叶传情久，终觅佳期成佳偶;秋雁送书来，诚邀亲友话亲情;
+佳人佳偶逢佳期，君至可添喜;亲友亲事话亲情，您来能增辉。
     `,
   },
-  wxVoice: { // 微信语音
-    boy: '',
-    girl: '',
-  },
   phone: { // 电话
-    boy: 18868875314,
-    girl: 18868875314,
+    boy: 18212341234,
+    girl: 18212341235,
   },
   wechat: { // 微信语音
     boy: {
       audio: require('./assets/audio/my/wechat.mp3'), // 语音
-      img: require('./assets/images/my/head1.jpg'), // 头像
+      img: randomImg({width: 100, height: 100}), // 头像
     },
     girl: {
       audio: require('./assets/audio/my/wechat-girl.mp3'),
-      img: require('./assets/images/my/head2.jpg'),
+      img: randomImg({width: 100, height: 100}),
     },
   },
   addr: { // 地图显示的地址
